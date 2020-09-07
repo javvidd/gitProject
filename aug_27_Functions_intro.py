4c8bef01aa5fb9c2c27fc6fbc659425cb567b713
javvidd <javvidd@gmail.com>
2020-09-06 22:34:01 -0400

    Finalised aug 27s functions introduction video.

--- aug_27_Functions_intro.py
+++ aug_27_Functions_intro.py
@@ -1,24 +1,50 @@
-import time
-
-# def first_tst():
-# #     """greets the usr"""
-# #     print("welcome home lol")
-# #
-# # def first_tst1(name):
-# #     """greeting the user with given name"""
-# #     print(f"welcome home {name}")
-#
-# first_tst()
-# first_tst1(input("please enter name: "))
+# functions
+# 2h
 
 
-def first_tst2(company, username=None):
-    """greeting the user with given name"""
-    if username is None:
-        print(f"heyy welcome back to {company.upper()}!")
+def greet():
+    print("welcome to the class")
+    print("__________")
+
+
+def greet_usr(x):
+    """greets the user with given name"""
+    print(f"welcome to {x} community")
+    print("__________")
+
+
+def greet_usr1(company, username):
+    """greets the user with given name"""
+    print(f"Hi {username.title()} welcome to {company.upper()} community ")
+    print("_________")
+
+
+# compName is an optional(keyword) and default val is google
+def greet_usr2(username, company_name="google"):
+    """greets the user with given name and keyword argument compName=google"""
+    print(f"Hi {username.title()} welcome to {company_name.upper()} community ")
+    print("__________")
+
+
+# compName argument is an optional arg and with no default val NONE
+def greet_usr3(username, company_name=None):
+    """greets the user with given name and keyword argument no default value"""
+    if company_name is None:
+        print(f"Hi {username.title()} welcome to yur NewHome")
     else:
-        print(f"{username.title()} welcome back to {company.upper()}!")
+        print(f"Hi {username.title()} welcome to {company_name.upper()} community ")
+    print("__________")
 
 
-first_tst2("ge", "yooo")
-first_tst2("pw")
+comp = "levelDown"
+greet()
+greet_usr("levelUp")  # calling function to be executed
+greet_usr(comp)
+greet_usr1("lgbt", "tony") # giving values by position
+greet_usr1(username="tony", company="lgbt")  # defining values by name
+greet_usr2("tony")  # function use default value which is google
+greet_usr3("polya", "fb")  # without second arg "fb" first condition will return
+greet_usr3("ali")
+
+# Package - directory with __init__.py empty file
+# Module - ...py file (python file)
