4436c55d656e1bbe773edf4cc73957350c419a34
javvidd <javvidd@gmail.com>
2020-09-12 09:25:00 -0400

    29 aug video 2h. 10min, classes introduction

---
+++ class_fundamentals.py
@@ -0,0 +1,17 @@
+# classes - chapter 9
+# class name always should be capital letter
+
+
+class Dog():
+    """this is general class about dog. """
+    # attributes (properties)
+    breed = ''
+    name = ''
+
+    # behaviour, methods
+    def bark(self):
+
+mydog = Dog  # mydog is the object of the class
+
+mydog.breed = "german shepard"
+mydog.name = "Rex"
